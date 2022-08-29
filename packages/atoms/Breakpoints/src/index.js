f119e68232ecb844f6bc1205208a102d3024f582
Abdul Rahman <abdul.rahman@happyfresh.com>
2022-08-30 00:04:48 +0700

    add mobile breakpoints

--- packages/atoms/Breakpoints/src/index.js
+++ packages/atoms/Breakpoints/src/index.js
@@ -1,11 +1,13 @@
 export const breakpointToMediaQuery = {
   DesktopAndUp: '(min-width: 1240px)',
-  TabletAndUp: '(min-width: 768px)'
+  TabletAndUp: '(min-width: 768px)',
+  MobileOnly: '(max-width: 767px)'
 }
 
 export const MediaQueryInput = {
   desktopAndUp: 'DesktopAndUp',
-  tabletAndUp: 'TabletAndUp'
+  tabletAndUp: 'TabletAndUp',
+  mobileOnly: 'MobileOnly'
 }
 
 export const mediaQueries = ({ content, breakpoints }) => `
