1f48aa4b6436328e38f54cb75e3df1aec36871fe
Abdul Rahman <abdul.rahman@happyfresh.com>
2022-08-29 14:49:10 +0700

    workspaces configuration and add row

---
+++ packages/atoms/Row/lib/cjs/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var React = _interopDefault(require('react'));
+
+var Row = function Row(_ref) {
+  var children = _ref.children,
+      _ref$styles = _ref.styles;
+  return /*#__PURE__*/React.createElement("div", null, children);
+};
+
+exports.Row = Row;
---
+++ packages/atoms/Row/lib/esm/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+var Row = function Row(_ref) {
+  var children = _ref.children,
+      _ref$styles = _ref.styles;
+  return /*#__PURE__*/React.createElement("div", null, children);
+};
+
+export { Row };
